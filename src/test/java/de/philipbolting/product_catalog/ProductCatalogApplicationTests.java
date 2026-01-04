6e13816d7bf7adb492ddfe4f095494907b4c3456
Philip Bolting <pb@philipbolting.de>
2026-01-04 14:29:13 +0100

    chore: Testcontainers setup for integration tests (CI preparation).

--- src/test/java/de/philipbolting/product_catalog/ProductCatalogApplicationTests.java
+++ src/test/java/de/philipbolting/product_catalog/ProductCatalogApplicationTests.java
@@ -2,11 +2,20 @@ package de.philipbolting.product_catalog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
+@Testcontainers
 @SpringBootTest
 class ProductCatalogApplicationTests {
 
-	@Test
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:18");
+
+    @Test
 	void contextLoads() {
 	}
 
