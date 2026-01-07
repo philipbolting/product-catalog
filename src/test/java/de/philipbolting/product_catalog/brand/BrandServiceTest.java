314cc5a037e114bbaf364bbddc0b5411d0707fe9
Philip Bolting <pb@philipbolting.de>
2026-01-07 11:43:52 +0100

    test: Brand service layer

---
+++ src/test/java/de/philipbolting/product_catalog/brand/BrandServiceTest.java
@@ -0,0 +1,50 @@
+package de.philipbolting.product_catalog.brand;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class BrandServiceTest {
+
+    @InjectMocks
+    private BrandService brandService;
+    @Mock
+    private BrandRepository brandRepository;
+
+    @Test
+    void createBrand_withDuplicateSlug_shouldThrowException() {
+        final var dto = new BrandDTO("some-slug", "Some Brand", "Some description");
+        when(brandRepository.findBySlug(dto.slug())).thenReturn(Optional.of(dto.toBrand()));
+        var exception = assertThrows(IllegalArgumentException.class, () -> brandService.createBrand(dto));
+        assertEquals("Brand with slug " + dto.slug() + " already exists", exception.getMessage());
+    }
+
+    @Test
+    void createBrand_withDuplicateName_shouldThrowException() {
+        final var dto = new BrandDTO("some-slug", "Some Brand", "Some description");
+        when(brandRepository.findByName(dto.name())).thenReturn(Optional.of(dto.toBrand()));
+        var exception = assertThrows(IllegalArgumentException.class, () -> brandService.createBrand(dto));
+        assertEquals("Brand with name " + dto.name() + " already exists", exception.getMessage());
+    }
+
+    @Test
+    void createBrand_withUniqueSlugAndName_shouldReturnBrand() {
+        final var dto = new BrandDTO("some-slug", "Some Brand", "Some description");
+        when(brandRepository.findBySlug(dto.slug())).thenReturn(Optional.empty());
+        when(brandRepository.findByName(dto.name())).thenReturn(Optional.empty());
+        when(brandRepository.save(dto.toBrand())).thenReturn(dto.toBrand());
+        var brand = brandService.createBrand(dto);
+        assertNotNull(brand);
+        assertEquals(dto.slug(), brand.getSlug());
+        assertEquals(dto.name(), brand.getName());
+        assertEquals(dto.description(), brand.getDescription());
+    }
+}
