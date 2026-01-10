7700e9783ca3cca6d614e1fb1fcd05d1a554d92d
Philip Bolting <pb@philipbolting.de>
2026-01-10 21:30:45 +0100

    style: Fix typo (uppercase parameter name)

--- src/main/java/de/philipbolting/product_catalog/brand/BrandRepository.java
+++ src/main/java/de/philipbolting/product_catalog/brand/BrandRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 interface BrandRepository extends CrudRepository<Brand, Long> {
-    Optional<Brand> findBySlug(String Slug);
-    Optional<Brand> findByName(String Slug);
+    Optional<Brand> findBySlug(String slug);
+    Optional<Brand> findByName(String slug);
 }
