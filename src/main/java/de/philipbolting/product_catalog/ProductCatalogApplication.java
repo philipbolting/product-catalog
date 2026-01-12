b70f3d5ff701c4657a573cdf2524fc03608f059a
Philip Bolting <pb@philipbolting.de>
2026-01-12 13:25:04 +0100

    fix: Error creating bean with name 'jpaAuditingHandler'
    Cannot resolve reference to bean 'jpaMappingContext' while setting constructor argument
    
    https://stackoverflow.com/questions/76540206/datajpatest-throws-error-creating-bean-with-name-jpaauditinghandler-occasio

--- src/main/java/de/philipbolting/product_catalog/ProductCatalogApplication.java
+++ src/main/java/de/philipbolting/product_catalog/ProductCatalogApplication.java
@@ -2,10 +2,8 @@ package de.philipbolting.product_catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class ProductCatalogApplication {
 
 	static void main(String[] args) {
