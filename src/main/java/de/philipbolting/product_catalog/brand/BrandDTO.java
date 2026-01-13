6d635aa49054d79ddaa4162de323ea6d7242d791
Philip Bolting <pb@philipbolting.de>
2026-01-13 16:58:35 +0100

    fix: Correct property name in error message

--- src/main/java/de/philipbolting/product_catalog/brand/BrandDTO.java
+++ src/main/java/de/philipbolting/product_catalog/brand/BrandDTO.java
@@ -9,8 +9,8 @@ public record BrandDTO(
         @Size(max = 50, message = "Slug must not be longer than {max} chars")
         @Pattern(regexp = "^[a-z0-9]+(?:-[a-z0-9]+)*$")
         String slug,
-        @NotEmpty(message = "Slug must not be empty")
-        @Size(max = 50, message = "Slug must not be longer than {max} chars")
+        @NotEmpty(message = "Name must not be empty")
+        @Size(max = 50, message = "Name must not be longer than {max} chars")
         String name,
         @Size(max = 2000, message = "Description must not be longer than {max} chars")
         String description) {
