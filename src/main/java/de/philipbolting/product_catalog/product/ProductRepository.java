d58ba05b0aaea63f3e2ed1a343926c3d6ff76306
Philip Bolting <pb@philipbolting.de>
2026-01-25 16:20:06 +0100

    feat: Product (repo, WIP)

--- src/main/java/de/philipbolting/product_catalog/product/ProductRepository.java
+++ src/main/java/de/philipbolting/product_catalog/product/ProductRepository.java
@@ -1,11 +1,10 @@
 package de.philipbolting.product_catalog.product;
 
-import de.philipbolting.product_catalog.brand.Brand;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
-    Optional<Brand> findBySlug(String slug);
-    Optional<Brand> findByName(String name);
+    Optional<Product> findBySlug(String slug);
+    Optional<Product> findByName(String name);
 }
